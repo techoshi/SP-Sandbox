fd8e655709ff6e9716ba637fed49feda6b18045e
Jose Risi <risi@emin-it.com>
2020-03-18 13:07:50 -0400

    Setting up modern Webpart

---
+++ src/SiteAssets/_spa/webparts/modern/spa-webpart/src/webparts/spaEnvironment/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ISpaEnvironmentWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'SpaEnvironmentWebPartStrings' {
+  const strings: ISpaEnvironmentWebPartStrings;
+  export = strings;
+}
